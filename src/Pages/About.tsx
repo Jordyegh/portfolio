47cc83d64f2501044ef4fa66909af68a8e50b809
jordyegh <jordyegh@gmail.com>
2022-09-14 22:40:19 +0200

    Added react router

---
+++ src/Pages/About.tsx
@@ -0,0 +1,5 @@
+function About(): JSX.Element {
+  return <h2>About me</h2>;
+}
+
+export default About;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/about', element: <About /> }
+]);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
